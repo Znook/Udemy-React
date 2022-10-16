c3819132ce5639344201700842ecbd48ca63d8cc
Jacob <Jacob.K289@outlook.com>
2022-10-16 15:58:48 +0200

    added useState to the onClick function

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+// imports below
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -5,16 +7,23 @@ import Card from "../UI/Card";
 const ExpenseItem = (props) => {
   // Created a new JS file for ExpenseDate. See for more info
 
+  // useState is called here
+  // We give useState 2 variables. One to that sets the title and another that stores what set to the title
+  const [title, setTitle] = useState(props.title);
+
+
   // Creating the onClick handler
   const clickHandler = () => {
-    console.log('Click handler');
+    // Here we allow our users to change the title with the setTitle property
+    setTitle('Updated!');
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate {...props} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
