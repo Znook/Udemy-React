ca1feac810623fded8fb559e180b1259176fd9be
Jacob <Jacob.K289@outlook.com>
2022-10-16 15:23:43 +0200

    added a click function to a button

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -5,6 +5,11 @@ import Card from "../UI/Card";
 const ExpenseItem = (props) => {
   // Created a new JS file for ExpenseDate. See for more info
 
+  // Creating the onClick handler
+  const clickHandler = () => {
+    console.log('Click handler');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate {...props} />
@@ -12,6 +17,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
